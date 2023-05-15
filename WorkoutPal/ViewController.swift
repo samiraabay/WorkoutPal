98d4ea7e8a5010b09b9a436e9f5e82de2083a64a
Samira Abayasekara <samiraabay@gmail.com>
2023-05-15 22:28:08 +0530

    Initial Commit

---
+++ WorkoutPal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutPal
+//
+//  Created by Samira Abayasekara on 2023-05-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
